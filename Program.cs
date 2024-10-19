c2fa91f10ee194c548d22a90f44527cf95a2a2e7
Nick <nikngeno@gmail.com>
2024-10-19 15:55:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ExtraCredit2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take user input
+            Console.Write("Enter the First Number: ");
+            int FirstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Second Number: ");
+            int SecondNumber = int.Parse(Console.ReadLine());
+
+            // Choose the operation to be performed through user input
+            Console.Write("Choose one operation to be performed from following list +, -, * and /: ");
+            string operation = Console.ReadLine();
+
+            double result;
+            // Choose the arithmetic operation to be performed
+
+            if (operation == "+")
+            {
+                result = FirstNumber + SecondNumber;
+            }
+            else if (operation == "-") 
+            {
+                result = FirstNumber - SecondNumber;
+            }
+            else if (operation == "*")
+            {
+                result = FirstNumber * SecondNumber;
+            }
+
+            else if(operation == "/")
+            {
+                result = FirstNumber / SecondNumber;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation selected.");
+                return;
+            }
+
+            Console.WriteLine($"Result:{FirstNumber} {operation} {SecondNumber} = {result}");
+        }
+    }
+}
